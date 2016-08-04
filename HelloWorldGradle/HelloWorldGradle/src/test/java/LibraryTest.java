2d5a9db2984295428fd28f75536f8773d5bd7e9d
bhabsaho <bhabani.a.sahoo@capgemini.com>
2016-08-04 15:13:58 +0530

    Update LibraryTest.java

--- HelloWorldGradle/HelloWorldGradle/src/test/java/LibraryTest.java
+++ HelloWorldGradle/HelloWorldGradle/src/test/java/LibraryTest.java
@@ -11,7 +11,7 @@ public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-        System.out.println("code has been changed")
+        System.out.println("code has been changed");
         //this code has been changed 
         
         
